850b2fb4cbd69bbcffb7fa1854b18223a494c74b
RandOm <takekazeX@ink2.top>
2026-05-31 23:36:47 +0800

    chore: bump version to 1.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
                 (System.getenv("GITHUB_REF_NAME")?.matches(Regex("^v[0-9.]+$")) == true)
         val isCI = System.getenv("GITHUB_ACTIONS") == "true"
 
-        val baseVersion = "1.3.3"
+        val baseVersion = "1.3.4"
         versionName = when {
             isStableRelease -> baseVersion
             isCI -> "$baseVersion-dev"
